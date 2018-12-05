cb4d4e8564381b47cbd3ba46b03001927ac49254
Markus <yimer-markus@gmx.de>
2018-12-05 16:02:27 +0100

    Added some scripts and LevelSelector scene

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour {
+
+    public string sceneToLoad;
+
+	// Use this for initialization
+	void Start () {
+		//if(Player)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
